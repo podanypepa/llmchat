b966f938184e942a63dbf06463bd93105c50e2e7
Josef Podany <podany.pepa@gmail.com>
2025-09-03 16:59:17 +0200

    anthropic send_request

---
+++ anthropic/send_request.go
@@ -0,0 +1,36 @@
+package anthropic
+
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"net/http"
+)
+
+func (c *Client) sendRequest(ctx context.Context, req *http.Request) (*http.Response, error) {
+	req.Header.Set("x-api-key", c.apiKey)
+	req.Header.Set("Content-Type", "application/json")
+	req.Header.Set("anthropic-version", "2023-06-01")
+
+	res, err := c.httpClient.Do(req)
+	if err != nil {
+		select {
+		case <-ctx.Done():
+			return nil, ctx.Err()
+		default:
+			return nil, err
+		}
+	}
+
+	if res.StatusCode != http.StatusOK {
+		defer res.Body.Close()
+		var errMessage any
+		if err := json.NewDecoder(res.Body).Decode(&errMessage); err != nil {
+			return nil, err
+		}
+		return nil, fmt.Errorf("api request failed: status Code: %d %s %s Message: %+v",
+			res.StatusCode, res.Status, res.Request.URL, errMessage)
+	}
+
+	return res, nil
+}
