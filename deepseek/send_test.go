9b94036aad97e09e3ba5b77bd86eb8f7ee8c230a
Josef Podany <podany.pepa@gmail.com>
2025-09-05 16:45:32 +0200

    deepseek send_test

---
+++ deepseek/send_test.go
@@ -0,0 +1,31 @@
+package deepseek
+
+import (
+	"context"
+	"fmt"
+	"os"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestSend(t *testing.T) {
+	client, err := NewClient(os.Getenv("DEEPSEEK_API_KEY"))
+	assert.NoError(t, err)
+	assert.NotNil(t, client)
+
+	req := &ChatCompletionRequest{
+		Model: ModelDeepSeekChat,
+		Messages: []ChatMessage{
+			{Role: "system", Content: "You are a pirate."},
+			{Role: "user", Content: "Hello!"},
+		},
+	}
+
+	resp, err := client.Send(context.TODO(), req)
+	assert.NoError(t, err)
+	assert.NotNil(t, resp)
+
+	fmt.Println(resp.Choices[0].Message.Content)
+	fmt.Println("total tokens:", resp.Usage.TotalTokens)
+}
