5b9523b012efe5021063a4b53c3722367a0e3b15
Josef Podany <podany.pepa@gmail.com>
2025-09-05 16:50:39 +0200

    deepseek simplesend and simplesend_test

---
+++ deepseek/simplesend_test.go
@@ -0,0 +1,23 @@
+package deepseek
+
+import (
+	"context"
+	"fmt"
+	"os"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestSimpleSend(t *testing.T) {
+	client, err := NewClient(os.Getenv("DEEPSEEK_API_KEY"))
+	assert.NoError(t, err)
+	assert.NotNil(t, client)
+
+	resp, err := client.SimpleSend(context.TODO(), "Hello!")
+	assert.NoError(t, err)
+	assert.NotNil(t, resp)
+
+	fmt.Println(resp.Choices[0].Message.Content)
+	fmt.Println("total tokens:", resp.Usage.TotalTokens)
+}
