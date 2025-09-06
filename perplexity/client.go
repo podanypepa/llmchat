55bab789d03e3557351498a190caf1c50dc78ede
Josef Podany <podany.pepa@gmail.com>
2025-09-06 11:03:40 +0200

    perplexity client

---
+++ perplexity/client.go
@@ -0,0 +1,57 @@
+// Package perplexity provides a client for interacting with the Mistral API.
+package perplexity
+
+import (
+	"cmp"
+	"fmt"
+	"net/http"
+)
+
+const (
+	// DefaultapiURL is the default base URL for the Mistral API.
+	DefaultapiURL = "https://api.mistral.ai"
+)
+
+// Client is the ChatGPT API client.
+type Client struct {
+	client *http.Client
+	config *Config
+}
+
+// Config holds the configuration for the Client.
+type Config struct {
+	BaseURL      string
+	APIKey       string
+	DefaultModel string
+}
+
+// NewClient creates a new Client with the provided API key.
+func NewClient(apikey string) (*Client, error) {
+	if apikey == "" {
+		return nil, fmt.Errorf("API key is required")
+	}
+
+	return &Client{
+		client: &http.Client{},
+		config: &Config{
+			BaseURL:      DefaultapiURL,
+			APIKey:       apikey,
+			DefaultModel: DefaultModel,
+		},
+	}, nil
+}
+
+// NewClientWithConfig creates a new Client with the provided configuration.
+func NewClientWithConfig(config *Config) (*Client, error) {
+	if config.APIKey == "" {
+		return nil, fmt.Errorf("API key is required")
+	}
+
+	config.BaseURL = cmp.Or(config.BaseURL, DefaultapiURL)
+	config.DefaultModel = cmp.Or(config.DefaultModel, DefaultModel)
+
+	return &Client{
+		client: &http.Client{},
+		config: config,
+	}, nil
+}
