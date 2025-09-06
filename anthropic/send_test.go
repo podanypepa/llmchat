ce0ba746b6541d43273765beb1d6b492de61689a
Josef Podany <podany.pepa@gmail.com>
2025-09-06 15:43:51 +0200

    fix anthropic send_test

--- anthropic/send_test.go
+++ anthropic/send_test.go
@@ -10,31 +10,55 @@ import (
 )
 
 func TestSend(t *testing.T) {
-	client, err := NewClient(os.Getenv("ANTHROPIC_API_KEY"))
-	assert.NoError(t, err)
-	assert.NotNil(t, client)
+	t.Run("SendOK", func(t *testing.T) {
+		client, err := NewClient(os.Getenv("ANTHROPIC_API_KEY"))
+		assert.NoError(t, err)
+		assert.NotNil(t, client)
 
-	req := &Request{
-		Model:     ModelClaude35Haiku,
-		System:    "You are a helpful assistant.",
-		MaxTokens: 300,
-		Messages: []Message{
-			{
-				Role:    "user",
-				Content: "Write a haiku about the sea.",
+		req := &Request{
+			Model:     ModelClaude35Haiku,
+			System:    "You are a helpful assistant.",
+			MaxTokens: 300,
+			Messages: []Message{
+				{
+					Role:    "user",
+					Content: "Write a haiku about the sea.",
+				},
 			},
-		},
-	}
+		}
 
-	resp, err := client.Send(context.TODO(), req)
-	assert.NoError(t, err)
-	assert.NotNil(t, resp)
-	if err != nil || resp == nil {
-		t.FailNow()
-	}
+		resp, err := client.Send(context.TODO(), req)
+		assert.NoError(t, err)
+		assert.NotNil(t, resp)
+		if err != nil || resp == nil {
+			t.FailNow()
+		}
 
-	fmt.Println(resp.Content[0].Text)
-	fmt.Println("input tokens:", resp.Usage.InputTokens)
-	fmt.Println("output tokens:", resp.Usage.OutputTokens)
-	fmt.Println("total tokens:", resp.Usage.InputTokens+resp.Usage.OutputTokens)
+		fmt.Println(resp.Content[0].Text)
+		fmt.Println("input tokens:", resp.Usage.InputTokens)
+		fmt.Println("output tokens:", resp.Usage.OutputTokens)
+		fmt.Println("total tokens:", resp.Usage.InputTokens+resp.Usage.OutputTokens)
+	})
+
+	t.Run("SendError", func(t *testing.T) {
+		client, err := NewClient(os.Getenv("ANTHROPIC_API_KEY"))
+		assert.NoError(t, err)
+		assert.NotNil(t, client)
+
+		req := &Request{
+			Model:     "dasdas",
+			System:    "You are a helpful assistant.",
+			MaxTokens: 300,
+			Messages: []Message{
+				{
+					Role:    "user",
+					Content: "Write a haiku about the sea.",
+				},
+			},
+		}
+
+		resp, err := client.Send(context.TODO(), req)
+		assert.NotNil(t, err)
+		assert.Nil(t, resp)
+	})
 }
