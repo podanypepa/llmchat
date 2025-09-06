9deac15d2bba43e4b6b9b317456cd4d0e368bb76
Josef Podany <podany.pepa@gmail.com>
2025-09-06 10:58:01 +0200

    mistral send_test

---
+++ mistral/send_test.go
@@ -0,0 +1,31 @@
+package mistral
+
+import (
+	"context"
+	"fmt"
+	"os"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestSend(t *testing.T) {
+	client, err := NewClient(os.Getenv("MISTRAL_API_KEY"))
+	assert.NoError(t, err)
+	assert.NotNil(t, client)
+
+	req := &ChatCompletionRequest{
+		Model: "mistral-small-2312",
+		Messages: []ChatMessage{
+			{Role: "system", Content: "You are a pirate."},
+			{Role: "user", Content: "Hello!"},
+		},
+	}
+
+	resp, err := client.Send(context.TODO(), req)
+	assert.NoError(t, err)
+	assert.NotNil(t, resp)
+
+	fmt.Println(resp.Choices[0].Message.Content)
+	fmt.Println("total tokens:", resp.Usage.TotalTokens)
+}
