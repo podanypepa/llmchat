d914d030e008ba7a2d17dc0b3424db1dddbd58a3
Josef Podany <podany.pepa@gmail.com>
2025-09-06 11:09:15 +0200

    perplexity send_test

---
+++ perplexity/send_test.go
@@ -0,0 +1,31 @@
+package perplexity
+
+import (
+	"context"
+	"fmt"
+	"os"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestSend(t *testing.T) {
+	client, err := NewClient(os.Getenv("PERPLEXITY_API_KEY"))
+	assert.NoError(t, err)
+	assert.NotNil(t, client)
+
+	req := &ChatCompletionRequest{
+		Model: DefaultModel,
+		Messages: []ChatMessage{
+			{Role: "system", Content: "You are a pirate."},
+			{Role: "user", Content: "Hello!"},
+		},
+	}
+
+	resp, err := client.Send(context.TODO(), req)
+	assert.NoError(t, err)
+	assert.NotNil(t, resp)
+
+	fmt.Println(resp.Choices[0].Message.Content)
+	fmt.Println("total tokens:", resp.Usage.TotalTokens)
+}
