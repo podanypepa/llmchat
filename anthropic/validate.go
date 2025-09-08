1d1a31e94fe15da56b9450a0d65e86ad5bdf3395
Josef Podany <podany.pepa@gmail.com>
2025-09-08 17:22:34 +0200

    anthropic validate

---
+++ anthropic/validate.go
@@ -0,0 +1,24 @@
+package anthropic
+
+import "fmt"
+
+func validate(r *ChatRequest) error {
+	if r.Model == "" {
+		return fmt.Errorf("model is required")
+	}
+	if r.MaxTokens < 1 {
+		return fmt.Errorf("max_tokens must be at least 1")
+	}
+	if len(r.Messages) == 0 {
+		return fmt.Errorf("at least one message is required")
+	}
+	for i, msg := range r.Messages {
+		if msg.Role == "" {
+			return fmt.Errorf("message %d: role is required", i)
+		}
+		if msg.Content == "" {
+			return fmt.Errorf("message %d: content is required", i)
+		}
+	}
+	return nil
+}
