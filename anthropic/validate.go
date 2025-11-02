9ee3b3b012d9d6d8aba98cded99f4aa75e659af5
Josef Podany <podany.pepa@gmail.com>
2025-11-02 17:14:03 +0100

    fix anthropic validate

--- anthropic/validate.go
+++ anthropic/validate.go
@@ -16,8 +16,23 @@ func validate(r *ChatRequest) error {
 		if msg.Role == "" {
 			return fmt.Errorf("message %d: role is required", i)
 		}
-		if msg.Content == "" {
+		if msg.Content == nil {
 			return fmt.Errorf("message %d: content is required", i)
+		}
+		switch content := msg.Content.(type) {
+		case string:
+			if content == "" {
+				return fmt.Errorf("message %d: content is required", i)
+			}
+		case []ContentBlock:
+			if len(content) == 0 {
+				return fmt.Errorf("message %d: content is required", i)
+			}
+		default:
+			// To support []any and other variations
+			if fmt.Sprintf("%v", msg.Content) == "[]" || fmt.Sprintf("%v", msg.Content) == "" {
+				return fmt.Errorf("message %d: content is required", i)
+			}
 		}
 	}
 	return nil
